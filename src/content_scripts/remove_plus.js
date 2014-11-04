2b4a0846afde7cc38a6a04b5b78c43673ccd19d5
Nichiatu <fristurt@hotmail.com>
2014-11-04 17:36:08 +0100

    forgot count

--- src/content_scripts/remove_plus.js
+++ src/content_scripts/remove_plus.js
@@ -14,6 +14,7 @@
                 
                 $.each(targets, function (index, element) {
                     elements.push($(this));
+                    count++;
                 });
 
                 break;
